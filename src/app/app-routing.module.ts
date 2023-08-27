8100daa3ee0e8233326adc2da46e9d5e509a421f
EmanAhamd <emanamra1999@gmail.com>
2023-08-27 12:54:51 +0300

    project Arch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(a => a.AuthModule)
+  },
+  { 
+    path: 'allPosts',
+    loadChildren: () => import('./posts/post/post.module').then(p => p.PostModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/posts/manage-post/manage-post.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManagePostRoutingModule } from './manage-post-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManagePostRoutingModule
+  ]
+})
+export class ManagePostModule { }
