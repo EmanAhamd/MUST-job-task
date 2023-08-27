cb3aaf4d971cda67caea7e9d91a2a10cc72f0acc
EmanAhamd <emanamra1999@gmail.com>
2023-08-27 14:03:24 +0300

    fix something in modules nesting

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'all', pathMatch:'full'},
+  
+  { 
+    path: 'all',
+    loadChildren: () => import('./post/post.module').then(p => p.PostModule)
+  },
+  
+  { 
+    path: 'managePosts',
+    loadChildren: () => import('./manage-post//manage-post.module').then(m => m.ManagePostModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
